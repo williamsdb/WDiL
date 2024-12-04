5743cc30b33bc05ccfd08013a1e83f5671555c4b
Neil Thompson <github@neilthompson.co.uk>
2024-12-04 19:52:09 +0000

    Turn on error reporting for notifications

--- src/notifications.php
+++ src/notifications.php
@@ -16,8 +16,8 @@
  */
 
 // turn off reporting of notices
-error_reporting(0);
-ini_set('display_errors', 0);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // Load Composer & parameters
 require __DIR__.'/vendor/autoload.php';
